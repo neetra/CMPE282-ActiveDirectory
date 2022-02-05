8f40f036ecee923ef80a190d79bdc932d4f99bbd
Netra Amrale <30404609+neetra@users.noreply.github.com>
2022-02-05 14:31:23 -0800

    add comment

--- PowerShellScripts/ExportMySQLDatatoCSV.ps1
+++ PowerShellScripts/ExportMySQLDatatoCSV.ps1
@@ -1,18 +1,46 @@
-﻿$path = $PSScriptRoot + "/employees.csv"
-$userObjects = import-csv -Path $path
-$currentUserCount  =(Get-ADUser -Filter * -SearchBase "OU=Users,OU=ronins,DC=ronins,DC=com").count
-$passwordString  = ConvertTo-SecureString "Temp@1234" -AsPlainText -Force
-Write-Output "Current number of Users Are "+ $currentUserCount  
-for ($i=0;$i -lt $userObjects.Count;$i++){
-$userObject =  $userObjects[$i]
-$department = $userObject.Department.Trim()
+﻿# Refernce : http://woshub.com/run-mysql-queries-from-powershell/
+#This script export data to CSV file
 
-$userName = ($userObject.FirstName + "." + $userObject.LastName).Trim()
+#Connect to my sql
+[system.reflection.assembly]::LoadWithPartialName("MySql.Data") 
+$mysql_server = "localhost"
+$mysql_user = "root"
+$mysql_password = ""
+$mysql_database = "employees"
+write-host "Create conection to $mysql_database"
+# Connect to MySQL database 'db1'
 
-$otherAttributes = @{'GivenName'=$userObject.FirstName;'Department'=$department}
-$msg  =  $i.ToString() + ") Creating user "  + $userName
+$cn = New-Object -TypeName MySql.Data.MySqlClient.MySqlConnection
+$cn.ConnectionString = "SERVER=$mysql_server;DATABASE=$mysql_database;UID=$mysql_user;PWD=$mysql_password"
+$MYSQLCommand = New-Object MySql.Data.MySqlClient.MySqlCommand
+$MYSQLDataAdapter = New-Object MySql.Data.MySqlClient.MySqlDataAdapter
+$MYSQLDataSet = New-Object System.Data.DataSet
+$MYSQLCommand.Connection=$cn
 
-Write-Output $msg
-#New-ADUser $userName -GivenName $userObject.FirstName -Surname $userObject.LastName -Department $department -AccountPassword $passwordString -Enabled $true
-#New-ADUser -Name $userName -OtherAttributes $otherAttributes
+# This sql scripts returns employees per department
+$currentDirc = $PSScriptRoot + "\FetchUsersByDepartment.sql"
+
+$sql = Get-Content $currentDirc 
+
+$OFS = "`r`n"
+$MYSQLCommand.CommandText = "$sql"
+$currentDirc = $PSScriptRoot + "\FetchUsersByDepartment.sql"
+$MYSQLDataAdapter.SelectCommand=$MYSQLCommand
+$NumberOfDataSets=$MYSQLDataAdapter.Fill($MYSQLDataSet, "data")
+$allemployees = [system.collections.arrayList]@()
+foreach($DataSet in $MYSQLDataSet.tables[0])
+{
+$employee =[pscustomobject]@{
+    FirstName = $DataSet.first_name
+    LastName = $DataSet.last_name
+    Department = $DataSet.dept_name
 }
+$allemployees.Add($employee)
+$newPath  = $PSScriptRoot + "\employees.csv"
+$employee | export-csv -Path $newPath -Append
+write-host "First NAme:" $DataSet.first_name  "Last Name:" $DataSet.last_name "Department :" $DataSet.dept_name
+}
+
+
+
+
---
+++ PowerShellScripts/PopulateADFromCSV.ps1
@@ -0,0 +1,16 @@
+﻿# This Script populate active directory from user in CSV file : Bulk Import
+
+$path = $PSScriptRoot + "/employees.csv"
+$userObjects = import-csv -Path $path
+$currentUserCount  =(Get-ADUser -Filter * -SearchBase "OU=Users,OU=ronins,DC=ronins,DC=com").count
+$passwordString  = ConvertTo-SecureString "Temp@1234" -AsPlainText -Force
+Write-Output "Current number of Users Are "+ $currentUserCount  
+for ($i=0;$i -lt $userObjects.Count;$i++){
+	$userObject =  $userObjects[$i]
+	$department = $userObject.Department.Trim()
+	# This is user identity
+	$userName = ($userObject.FirstName + "." + $userObject.LastName).Trim()
+	$msg  =  $i.ToString() + ") Creating user "  + $userName
+	Write-Output $msg
+	New-ADUser $userName -GivenName $userObject.FirstName -Surname $userObject.LastName -Department $department -AccountPassword $passwordString -Enabled $true
+}
